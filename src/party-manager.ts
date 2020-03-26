a349a0d1ed7e98c6dc134fdfc0be6a242fe28c13
Andy Perlitch <andyperlitch@gmail.com>
2020-03-25 23:22:19 -0700

    added to party manager

--- src/party-manager.ts
+++ src/party-manager.ts
@@ -1,11 +1,54 @@
 import { createWorker } from 'mediasoup'
-import { Worker } from 'mediasoup/lib/types'
+import { Worker, RtpCodecCapability, Router } from 'mediasoup/lib/types'
+
+interface IParty {
+  router: Router
+}
+
+interface IPartyManager {
+  createParty: () => Promise<IParty>
+}
 
 let worker: Worker
 
-async function initPartyManager() {
+export async function initPartyManager(): Promise<IPartyManager> {
+  // create the worker (C++ process)
   worker = await createWorker()
-  const router = worker.createRouter({
-    mediaCodecs: {},
+
+  // return manager interface
+  return {
+    createParty,
+  }
+}
+
+async function createParty(): Promise<IParty> {
+  // init the mediasoup router
+  const router = await worker.createRouter({
+    mediaCodecs: getMediaCodecs(),
   })
+
+  return {
+    router,
+  }
+}
+
+function getMediaCodecs(): RtpCodecCapability[] {
+  return [
+    {
+      kind: 'audio',
+      mimeType: 'audio/opus',
+      clockRate: 48000,
+      channels: 2,
+    },
+    {
+      kind: 'video',
+      mimeType: 'video/H264',
+      clockRate: 90000,
+      parameters: {
+        'packetization-mode': 1,
+        'profile-level-id': '42e01f',
+        'level-asymmetry-allowed': 1,
+      },
+    },
+  ]
 }
